6ee1a422318b3a43da8fbf7a56fdb2175ccdcd82
Juan COYLA <jcoyla123@gmail.com>
2019-11-28 23:20:53 +0100

    add problem explain

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * Given a matrix of n*n. Each cell contain 0, 1, -1.
+0 denotes there is no diamond but there is a path.
+1 denotes there is diamond at that location with a path
+-1 denotes that the path is blocked.
+Now you have start from 0,0 and reach to last cell & then return back to 0,0 collecting maximum no of diamonds.
+While going to last cell you can move only right and down.
+While returning back you can move only left and up.
+
+ */
+
 function collectMax(mat) {
     // Write your code here<
     let currentMap = clone(mat);
