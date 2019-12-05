7c5df5c809217f58541db00cfa588ee1373d0238
Juan COYLA <jcoyla123@gmail.com>
2019-12-05 22:11:56 +0100

    add dubstep kata

--- dojos.js
+++ dojos.js
@@ -302,7 +302,34 @@ Let's assume that a song consists of some number of words (that don't contain WU
 For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
 
 Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+
+the best solution is to use the regex pattern \(WUB)+\g  the + () the pattern to allow WUB repeated and g means to 
+find all occurange in the string
  */
+
+
+function songDecoder(song){
+ 
+    const songFiltered = song.replace(/WUB/g, ' ').trim();
+    return removeSpaces(songFiltered);
+    
+    
+    }
+    
+    function removeSpaces(string) {
+      const removed = [];
+      const stringToFilter = string.split('');
+      
+      let previous = '';
+      
+      stringToFilter.forEach(letter => {
+        if (letter !== previous ||  letter !== ' ' ) {
+          removed.push(letter);
+          previous = letter;
+        }
+      });
+      return removed.join('');
+    }
 
  
 
@@ -312,3 +339,15 @@ module.exports = {
     collectMax,
 };
 
+
+/**
+ * 
+ * AA  CA
+ * 
+ * previous = ''
+ * current = A
+ * 
+ * false OK 
+ * 
+ * AA CA
+ */
