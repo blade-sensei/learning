312b5e94764232433d670bdb366354b055f381a7
Juan COYLA <jcoyla123@gmail.com>
2019-12-08 23:31:32 +0100

    high wordscore

--- dojos.js
+++ dojos.js
@@ -395,6 +395,47 @@ The jump length is calculated by (mountain height * speed * 9) / 10. Jump length
         }
     }
 
+    /**
+     * 
+     * Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+     */
+
+    function high(x){
+        const words = x.split(' ');
+
+        let currentMaximalScoreWinner = {
+            word: '',
+            score: 0,
+        }
+
+        words.forEach(word => {
+            const score = getWordScore(word);
+            if (score > currentMaximalScoreWinner.score) {
+                currentMaximalScoreWinner.score = score;
+                currentMaximalScoreWinner.word = word;
+            }
+        })
+
+        return currentMaximalScoreWinner.word;
+    }
+
+    function getWordScore(word) {
+        //position in alphabet
+        const letters = [...word];
+        let totalScore = 0;
+        letters.forEach(letter => {
+            const score = letter.charCodeAt(0) - 96;
+            totalScore = totalScore + score;
+        })
+        return totalScore;
+    }
+
 module.exports = {
     maxGapBinary,
     rotateArray,
@@ -402,6 +443,7 @@ module.exports = {
     songDecoder,
     DNAStrand,
     skiJump,
+    high,
 };
 
 
--- test/test.js
+++ test/test.js
@@ -4,9 +4,8 @@ const dojos = require('../dojos');
 describe('coding test', () => {
     
     it('test', () => {
-        assert.equal(dojos.skiJump(['*']), '1.35 metres: He\'s crap!');
-        assert.equal(dojos.skiJump(['*', '**', '***']), '12.15 metres: He\'s ok!');
-        assert.equal(dojos.skiJump(['*', '**', '***', '****', '*****', '******']), '48.60 metres: He\'s flying!');
-        assert.equal(dojos.skiJump(['*', '**', '***', '****', '*****', '******', '*******', '********']), '86.40 metres: Gold!!');
+        assert.equal(dojos.high('man i need a taxi up to ubud'), 'taxi');
+        assert.equal(dojos.high('what time are we climbing up the volcano'), 'volcano'); 
+        assert.equal(dojos.high('take me to semynak'), 'semynak');  
     })
 });
