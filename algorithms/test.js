1c3ebd01a48b0815c5deca099270938a1c8536c4
Juan COYLA <jcoyla123@gmail.com>
2019-12-26 17:03:32 +0100

    (data): implement dfs for graphs

--- algorithms/test.js
+++ algorithms/test.js
@@ -77,6 +77,8 @@ class Graph {
     *bfs(first) {
         const visited = new Map();
         const visitList = new Queue();
+
+        //we add this to store visited nodes if some is already visited collid
         visitList.enqueue(first);
 
         while(!visitList.isEmpty()) {
@@ -85,6 +87,22 @@ class Graph {
                 yield node;
                 visited.set(node);
                 node.getAdjacents().forEach(adj => visitList.enqueue(adj));
+            }
+        }
+    }
+
+    *dfs(first) {
+        const visited = new Map();
+        const visitList = new Stack();
+        // add the first node this will be remove after first iteration.
+        // other child nodes will be added in foreach loop
+        visitList.push(first);
+        while (!visitList.isEmpty()) {
+            const node = visitList.pop()
+            if (node && !visited.has(node)) {
+                yield node;
+                visited.set(node);
+                node.getAdjacents().forEach(node => visitList.push(node));
             }
         }
     }
@@ -108,12 +126,52 @@ class Queue {
     }
 }
 
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    push(element) {
+        this.array.push(element);
+    }
+    
+    pop(element) {
+        return this.array.pop();
+    }
+    isEmpty() {
+        return this.array.length === 0;
+    }
+}
+
 Graph.UNDIRECTED = Symbol('undirected grapth');
 Graph.DIRECTED = Symbol('directed grapth');
 
+const graphBfs = new Graph(Graph.UNDIRECTED);
+
+const [first] = graphBfs.addEdge(1, 2);
+graphBfs.addEdge(1, 3);
+graphBfs.addEdge(1, 4);
+graphBfs.addEdge(5, 2);
+graphBfs.addEdge(6, 3);
+graphBfs.addEdge(7, 3);
+graphBfs.addEdge(8, 4);
+graphBfs.addEdge(9, 5);
+graphBfs.addEdge(10, 6);
+
+bfsFromFirst = graphBfs.bfs(first);
+
+bfsFromFirst.next().value.value; // 1
+bfsFromFirst.next().value.value; // 2
+bfsFromFirst.next().value.value; // 3
+bfsFromFirst.next().value.value; // 4
+// ...
+
+
+// test dfs implementation
+
 const graph = new Graph(Graph.UNDIRECTED);
 
-const [first] = graph.addEdge(1, 2);
+const [firstOf] = graph.addEdge(1, 2);
 graph.addEdge(1, 3);
 graph.addEdge(1, 4);
 graph.addEdge(5, 2);
@@ -123,10 +181,17 @@ graph.addEdge(8, 4);
 graph.addEdge(9, 5);
 graph.addEdge(10, 6);
 
-bfsFromFirst = graph.bfs(first);
+// iterator with first value
+const dfsFromFirst = graph.dfs(firstOf);
 
-bfsFromFirst.next().value.value; // 1
-bfsFromFirst.next().value.value; // 2
-bfsFromFirst.next().value.value; // 3
-bfsFromFirst.next().value.value; // 4
-// ...
+// get array from this iterator
+const order = Array.from(dfsFromFirst);
+const values = order.map(node => node.value);
+console.log(values); // [1, 4, 8, 3, 7, 6, 10, 2, 5, 9]
+
+/**
+ *          1
+ *   2      3       4
+ * 5       6  7       8
+ * 
+ */
