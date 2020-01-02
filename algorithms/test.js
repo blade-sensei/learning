e8b3ad80451ffd441d4781148fec7936b464767a
Juan COYLA <jcoyla123@gmail.com>
2020-01-02 16:54:45 +0100

    algo: two sum improve time complexity with hashmap

---
+++ algorithms/test.js
@@ -0,0 +1,50 @@
+var twoSum = function (nums, target) {
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            const sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+};
+
+/** 
+ * [2, 7, 11, 15, 12]
+ * 
+ */
+
+
+/**
+ * This solution has time complexity of O(n) vs O(n^2)
+ * We also need to sub to get complement if we use sum we need
+ * know before both value to get target.
+ * We use hash map to get complement
+ * @param {} nums 
+ * @param {*} target 
+ */
+var twoSumHash = function (nums, target) {
+    // hash map
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+        map[value] = i;
+    }
+
+    // iteration
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        const hashComplement = map[complement];
+
+        if (hashComplement && hashComplement !== i) {
+            return [i, hashComplement];
+        }
+
+    }
+
+    return [];
+}
+
+twoSumHash([2, 7, 11, 15, 12], 9);
