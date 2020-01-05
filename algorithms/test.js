b33f84773dde1a803a0bce20b5d62c1150131b25
Juan COYLA <jcoyla123@gmail.com>
2020-01-05 22:29:27 +0100

    algo: leetcode reverse digits first version using 10 mudulo

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,30 +1,25 @@
-/**
- * 
- * @param {Array} array 
- */
-function reverse(array) {
-    // know pivot
-    
-    const pivot = Math.floor(array.length / 2);
-    for (let i = 0; i < pivot; i++) {
-        const oppositeIndex = (array.length - 1) - i;
-        const currentValue = array[i];
-        array[i] = array[oppositeIndex];
-        array[oppositeIndex] = currentValue;
+function lengthDigits(number) {
+    let counterDigits = 0;
+    while (number > 0) {
+        counterDigits++;
+        number = Math.floor(number / 10);
     }
-    return array;
-    // iterate from 0 to pivot (not included)
-    // oposite symetric side index
-    // save left side value
-    // assign opposite symetric side value to current index
-    // assign to symetric side the saved value
-
-    //return reversed array
+    return counterDigits;
 }
 
-function isEven(number) {
-    return number % 2 === 0
-}
+function reverse (x) {
 
-reverse([1, 2, 3, 4]);
-reverse([1, 2, 3, 4, 5]);
+    let numberToReverse = x;
+    let numberOfDigits = lengthDigits(numberToReverse);
+    let reversedNumber = 0;
+    while (numberToReverse > 0) {
+        const lastDigit = numberToReverse % 10;
+        reversedNumber += lastDigit * (10 ** (numberOfDigits - 1));
+        numberToReverse = Math.floor(numberToReverse / 10);
+        numberOfDigits--;
+    }
+    return reversedNumber;
+};
+
+reverse(123);
+
