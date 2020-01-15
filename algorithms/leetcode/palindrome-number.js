f9b6607047f020b0c9d9bd4833d216cdcb545107
Juan COYLA <jcoyla123@gmail.com>
2020-01-15 22:41:57 +0100

    algo: leetcode palindrome-number add new version half no array convert string

--- algorithms/leetcode/palindrome-number.js
+++ algorithms/leetcode/palindrome-number.js
@@ -1,42 +1,36 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false;
-    } else {
-        const reverse = getReverse(x);
-        if (reverse === x) {
-            return true;
-        }
+    let number = x;
+    let reverse = 0;
+
+    if (number === 0) {
+        return true;
+    }
+
+    if (number < 0) {
         return false;
     }
-  
-  };
-  
-  
-  function getLength(number) {
-      let length = 0;
-  
-      while (number) {
-          length++;
-          number = Math.trunc(number/10);
-      }
-      return length;
-  }
-  
-  function getReverse(number) {
-      let length = getLength(number);
-      let multiplierIteration = 10 ** (length - 1);
-      let reverse = 0;
-      while (number) {
-          const lastDigit = number % 10;
-          reverse += lastDigit * multiplierIteration;
-          number = Math.trunc(number / 10);
-          multiplierIteration /= 10
-      }
-      return reverse;
-  }
-  
-  const test = isPalindrome(121);
+    if (getLastDigit(number) === 0) {
+        return false;
+    }
+    
+    while (reverse < number) {
+        reverse = reverse * 10 + getLastDigit(number);
+        number = Math.trunc(number / 10);
+    }
+
+    if (reverse === number) {
+        return true;
+    }
+
+    return Math.trunc(reverse / 10) === number;
+}
+
+function getLastDigit(number) {
+    return number % 10;
+}
+
+/**
+ * description 
+ * Coud you solve it without converting the integer to a string?
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ */
