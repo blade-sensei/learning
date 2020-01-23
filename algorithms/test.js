94dc2995fc1b2969af2c367b00d2681eb18e9d85
Juan COYLA <jcoyla123@gmail.com>
2020-01-23 20:29:44 +0100

    algo: test convert romain number to dec

---
+++ algorithms/test.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ function romanToInt (s) {
+    let substraction = 0;
+    const symbols = s.split('');
+    const romanNumber = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'L': 1000
+    }
+    const combinations = {
+        'I': {
+            'V': 4,
+            'X': 9,
+        },
+        'X': {
+            'L': 40,
+            'C': 90,
+        },
+        'C': {
+            'D': 400,
+            'M': 900,
+        }
+    }
+
+    if (symbols.length > 1) {
+        substraction = substractionValue(symbols, combinations);
+        if (substraction !== 0) {
+            symbols.splice(0, 2);
+        }
+    }
+    return substraction + getDecimalValue(symbols, romanNumber);
+};
+
+function substractionValue([first, second], combinations) {
+    if (combinations[first] && combinations[first][second]) {
+        return combinations[first][second]
+    }
+    return 0;
+}
+
+function getDecimalValue(romanSymbols, mapper) {
+    let total = 0;
+    for (symbol of romanSymbols) {
+        total += mapper[symbol];
+    }
+    return total;
+}
+
+romanToInt('VI');
