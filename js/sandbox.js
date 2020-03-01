02e23117789babfdd0bfdd4c10fbb569bfbde853
Juan COYLA <jcoyla123@gmail.com>
2020-03-01 16:22:30 +0100

    lesson: async await

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function fakePromise(value) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -6,6 +8,25 @@ function fakePromise(value) {
     }, 1000);
   });
 }
+
+function promise2() {
+  return new Promise((resolve, reject) => {
+    reject('error');
+  });
+}
+
+function promise3() {
+  return new Promise((resolve, reject) => {
+    resolve('promise 3')
+  })
+}
+
+async function all() {
+  const test = await Promise.all([fakePromise, promise2, promise3])
+  console.log(test);
+}
+
+all();
 
 //callback du setTimeOut est mis en Callback Queue
 //Ressoue place Resolve et Reject en execution
@@ -20,7 +41,7 @@ function asynchroneHappySad(value) {
   console.log('never executed');
 }
 
-asynchroneHappySad(true);
+//asynchroneHappySad(true);
 
 //asynchroneHappySad(true);
 
@@ -52,4 +73,88 @@ function promise() {
 //   console.log('end of callback')
 // })
 
+/**
+ * --- Files ---
+ */
+
+
+ function readFileHello(callback) {
+   fs.readFile( __dirname + '/hello.txt', { encoding: 'utf8' }, (error, data) => {
+     console.log('i finished the first readfile');
+     callback(data);
+     console.log('i wait for the callback');
+
+   })
+ }
+
+ function responseToHello(data, callback) {
+   console.log(`i will write ${data}`);
+   fs.writeFile(__dirname + '/response.txt',  data, () => {
+     console.log('i responded');
+     callback(data);
+   })
+ }
+
+//readFileHello(responseToHello);
+//readFileHelloPromise()
+
+function readFileHelloPromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/hello.txt', { encoding: 'utf8' }, (error, data) => {
+      resolve(data);
+    })
+  })
+}
+
+function changeHelloFile(textHello) {
+  const addData = textHello + ' i added some text';
+  fs.writeFile(__dirname + '/hello.txt', addData, (error) => {
+    console.log('i added data');
+  })
+}
+
+function responseToHelloPromise(data) {
+  console.log(`i will write ${data}`);
+  fs.writeFile(__dirname + '/response.txt',  data, () => {
+    console.log('i responded');
+  });
+}
+
+async function under() {
+  await fakePromise(true)
+  console.log('coucou');
+  return 'hello'
+}
+
+async function over() {
+  const underData = await under();
+  const changed = underData + ' changed';
+  console.log(changed);
+  return changed;
+}
+
+function hello(data) {
+  console.log(`data is here: ${data}`);
+}
+
+(async function() {
+  const helloChange = await over();
+  hello(helloChange);   
+})()
+
+// readFileHello(function (data) {
+//   console.log(`i will write ${data}`);
+//   responseToHello(data, changeHelloFile)
+// });
+
+//  readFileHelloPromise()
+//  .then(textHello => {
+//    responseToHelloPromise(textHello);
+//  })
+//  .then(textHello => {
+//    changeHelloFile(textHello);
+//  })
+//  .catch((error) => console.log(error));
+
 console.log('after callback');
+
