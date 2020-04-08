84312076c79cbf1f776ffce60ce901076066f6f5
Juan COYLA <jcoyla123@gmail.com>
2020-04-08 22:49:19 +0200

    algo: kadane algorithm second approch with negative subarray

--- algorithms/leetcode/challenge-3-04-20.js
+++ algorithms/leetcode/challenge-3-04-20.js
@@ -4,18 +4,29 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  if (nums.length === 1) return nums[0];
   let currentMax = 0;
-  let globalMax = Infinity;
+  let globalMax = -Infinity
   
   for (let number of nums) {
-    currentMax = Math.max(currentMax + number, number);
-  
+    currentMax = currentMax + number;
     globalMax = Math.max(globalMax, currentMax);
+    currentMax = Math.max(currentMax, 0);
   }
   return globalMax
 };
 
-const params = [1]
+const params = [3]
 const test = maxSubArray(params);
 console.log(test);
+
+/**
+ * summary
+ * 
+ * another to be sure the first iteration/smallest subarray 
+ * will not be wrong by compare globalMax = 0 and currentMax a negative number
+ * we should make sure that currentMax will not be negative
+ * To do that modify currentMax by
+ * currentMax = Math.max(currentMax + number, 0) -> If currentMax + number is negative
+ * the crrentMax set will prefer 0.
+ * 
+ */
