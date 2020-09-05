cb5864486eb3d4fa0c7c1ddc876e336c50529e16
Juan COYLA <jcoyla123@gmail.com>
2020-09-05 17:09:44 +0200

    algo: remove duplicates

---
+++ algorithms/remove-duplicates.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+
+
+var removeDuplicates = function(nums) {
+  let globalIndex = 0;
+  while(hasDuplicates(nums)) {
+      const startIndexDuplicates = globalIndex;
+      const uniqueValue = nums[globalIndex];
+      
+      while(isNextTheSame(uniqueValue, startIndexDuplicates, nums)) {
+          nums.splice(startIndexDuplicates, startIndexDuplicates + 1);
+      }
+      
+      globalIndex++;
+  }
+  return nums.length;
+};
+
+function isNextTheSame(value, startIndex, nums) {
+  return (nums[startIndex + 1] === value)
+}
+
+function hasDuplicates(nums) {
+  for (let numIndex = 0; numIndex < nums.length - 1; numIndex++) {
+      if (nums[numIndex] === nums[numIndex + 1]) return true;
+  }
+  return false;
+}
