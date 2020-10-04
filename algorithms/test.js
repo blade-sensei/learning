0321aa509f4724dbd9e3ee2acbef6fc38886b428
Juan COYLA <jcoyla123@gmail.com>
2020-10-04 18:48:39 +0200

    algo: hackerrank minimumSwaps

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,44 +1,29 @@
-
-const VALID_OCCURANCE_LETTER = 'a'
-
-function isOccuranceOfA(letter) {
-    return letter === VALID_OCCURANCE_LETTER;
+function hasCorrectPosition(position, number) {
+    return position === number - 1;
 }
 
-function getNumberOfOccurance(word, maxLength) {
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr) {
+    if (arr.length === 1) return 0;
+
+    const list = [...arr];
+    let swap = 0;
     let cursor = 0;
-    let occurancesOfLetterA = 0;
-    while(cursor < maxLength) {
-        const currentLetter = word.charAt(cursor);
-        if (isOccuranceOfA(currentLetter)) {
-            occurancesOfLetterA++;
+    while (cursor < list.length - 1); {
+        const number = list[cursor];
+        if (!hasCorrectPosition(cursor, number)) {
+            const correctPosition = number - 1;
+            const tempo = list[correctPosition];
+            list[correctPosition] = number;
+            list[cursor] = tempo;
+            swap++
+            cursor = 0
+        } else {
+            cursor++
         }
-        cursor++;
-    }
-    return occurancesOfLetterA;
-}
-
-function repeatedString(s, n) {
-    const limitWord = s.length;
-    let cursor = 0;
-    let occurancesOfLetterA = 0;
-    let minimumOccurances = getNumberOfOccurance(s, limitWord);
-
-    const numberOfRepetions = n % limitWord;
-
-    if (numberOfRepetions === 0) {
-        occurancesOfLetterA =  minimumOccurances * (n / limitWord);
-    } else {
-        let firstRepetions = n / limitWord;
-        firstRepetions = Math.trunc(firstRepetions);
-        const occurances = getNumberOfOccurance(s, numberOfRepetions);
-        occurancesOfLetterA = (minimumOccurances * firstRepetions) + occurances;
     }
 
-    return occurancesOfLetterA;
+    return swap;
 }
 
-const test = 'kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm';
-const limit = 736778906400;
-
-console.log(repeatedString(test, limit));
+console.log(minimumSwaps([4,3,1,2]));
