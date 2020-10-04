4b154935674ca7bdbede0a853270b7fd41b1647a
Juan COYLA <jcoyla123@gmail.com>
2020-10-04 15:45:17 +0200

    algo: hackerrank - repeatedString

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,18 +1,44 @@
-/**
- * @param {number[]} nums
- * @return {number}
- * https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
- */
-var singleNumber = function(nums) {
-    let found = [];
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (found.includes(nums[i])) {
-            found = found.filter((num) => num !== nums[i]);
-        } else {
-            found.push(nums[i]);
+
+const VALID_OCCURANCE_LETTER = 'a'
+
+function isOccuranceOfA(letter) {
+    return letter === VALID_OCCURANCE_LETTER;
+}
+
+function getNumberOfOccurance(word, maxLength) {
+    let cursor = 0;
+    let occurancesOfLetterA = 0;
+    while(cursor < maxLength) {
+        const currentLetter = word.charAt(cursor);
+        if (isOccuranceOfA(currentLetter)) {
+            occurancesOfLetterA++;
         }
+        cursor++;
     }
-    console.log(found[0]);
-    return found[0]
-};
+    return occurancesOfLetterA;
+}
+
+function repeatedString(s, n) {
+    const limitWord = s.length;
+    let cursor = 0;
+    let occurancesOfLetterA = 0;
+    let minimumOccurances = getNumberOfOccurance(s, limitWord);
+
+    const numberOfRepetions = n % limitWord;
+
+    if (numberOfRepetions === 0) {
+        occurancesOfLetterA =  minimumOccurances * (n / limitWord);
+    } else {
+        let firstRepetions = n / limitWord;
+        firstRepetions = Math.trunc(firstRepetions);
+        const occurances = getNumberOfOccurance(s, numberOfRepetions);
+        occurancesOfLetterA = (minimumOccurances * firstRepetions) + occurances;
+    }
+
+    return occurancesOfLetterA;
+}
+
+const test = 'kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm';
+const limit = 736778906400;
+
+console.log(repeatedString(test, limit));
