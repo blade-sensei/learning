7bfbe5e35ba83a30045f1406a8084c5c61ce537d
Juan COYLA <jcoyla123@gmail.com>
2020-10-05 09:44:19 +0200

    kits

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,29 +1,38 @@
-function hasCorrectPosition(position, number) {
-    return position === number - 1;
-}
+function minimumKits(totalKits, kitsStrength) {
+    const chooseKits = [];
+    let aliceTotalStrength = 0;
+    let bobTotalStrength = 0;
+    let kitIndex = 0;
+    while (bobTotalStrength <= aliceTotalStrength) {
+        aliceTotalStrength = sum(kitsStrength.slice(kitIndex + 1));
+        const kit = kitsStrength[kitIndex];
+        bobTotalStrength = bobTotalStrength + kit;
+        chooseKits.push(kit);
+        kitIndex++;
 
-// Complete the minimumSwaps function below.
-function minimumSwaps(arr) {
-    if (arr.length === 1) return 0;
-
-    const list = [...arr];
-    let swap = 0;
-    let cursor = 0;
-    while (cursor < list.length - 1); {
-        const number = list[cursor];
-        if (!hasCorrectPosition(cursor, number)) {
-            const correctPosition = number - 1;
-            const tempo = list[correctPosition];
-            list[correctPosition] = number;
-            list[cursor] = tempo;
-            swap++
-            cursor = 0
-        } else {
-            cursor++
-        }
     }
-
-    return swap;
+    return chooseKits.length;
 }
 
-console.log(minimumSwaps([4,3,1,2]));
+
+function getVariableInArray(testInput) {
+    const variables = testInput.split('\n');
+    return variables;
+}
+
+function convertVariablesToIntegers(variables) {
+    return variables.map((variable) => Number(variable));
+}
+
+function splitVariables(variables) {
+    const [testCasesLength, ...testCasesVariables] = variables;
+    return [testCasesLength, testCasesVariables];
+}
+
+console.log(minimumKits(5, [5, 3, 4, 1, 2]));
+
+function sum (kits) { 
+    return kits.reduce( (a, b)  => {
+    return a + b;
+}, 0);
+}
