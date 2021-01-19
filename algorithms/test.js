ec1f2780520174ac107de565f2d8f38299f856fb
Juan COYLA <jcoyla123@gmail.com>
2021-01-19 08:49:01 +0100

    test: conflicts trailing comma

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,38 +1,6 @@
-function minimumKits(totalKits, kitsStrength) {
-    const chooseKits = [];
-    let aliceTotalStrength = 0;
-    let bobTotalStrength = 0;
-    let kitIndex = 0;
-    while (bobTotalStrength <= aliceTotalStrength) {
-        aliceTotalStrength = sum(kitsStrength.slice(kitIndex + 1));
-        const kit = kitsStrength[kitIndex];
-        bobTotalStrength = bobTotalStrength + kit;
-        chooseKits.push(kit);
-        kitIndex++;
+const conflicts = {
+  first: 'first',
+  test: 'test'
+};
 
-    }
-    return chooseKits.length;
-}
-
-
-function getVariableInArray(testInput) {
-    const variables = testInput.split('\n');
-    return variables;
-}
-
-function convertVariablesToIntegers(variables) {
-    return variables.map((variable) => Number(variable));
-}
-
-function splitVariables(variables) {
-    const [testCasesLength, ...testCasesVariables] = variables;
-    return [testCasesLength, testCasesVariables];
-}
-
-console.log(minimumKits(5, [5, 3, 4, 1, 2]));
-
-function sum (kits) { 
-    return kits.reduce( (a, b)  => {
-    return a + b;
-}, 0);
-}
+console.log(conflicts);
