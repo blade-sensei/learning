1567dc668ab60ace09c54052c03983adbabcb5d1
Aurore Godard <aurore_loistron@msn.com>
2024-02-01 13:59:52 +0100

    ajout prompt

--- lessons/practice/jeuDeLoie-AG/jeuDeLoie-AG.js
+++ lessons/practice/jeuDeLoie-AG/jeuDeLoie-AG.js
@@ -1,4 +1,6 @@
-// mise en place
+const prompt = require('prompt');
+const colors = require("@colors/colors/safe");
+
 function creerPlateauJeuOie(positionPion) {
     const nombreCases = 64;
     let plateau = [];
@@ -13,80 +15,110 @@ function creerPlateauJeuOie(positionPion) {
     return plateau;
 }
 
-
 let positionPion = 0; 
-creerPlateauJeuOie(positionPion);
+
+function affichePlateau () {
+    let plateauDeJeu = creerPlateauJeuOie(positionPion);
+    console.log(plateauDeJeu);
+}
 
 function lancerDes() {
     let de1 = Math.floor(Math.random() * 6) + 1;
     let de2 = Math.floor(Math.random() * 6) + 1;
-    return 50 + 8;
+    return de1 + de2;
 }
 
-// Jouer
-// TODO: faire une question pour lancer les dés
+function casesSpeciales (de1, resultatDes, positionPion) {
+    let nouvellePosition = positionPion;
+
+    if (tour === 1 && resultatDes === 9) {
+        if (de1 === 4 || de1 === 5) {
+            nouvellePosition = 53
+            console.log(colors.blue('Vous allez directement à la case:', nouvellePosition))
+            return nouvellePosition; 
+        } 
+        else 
+        {
+            nouvellePosition = 26
+            console.log(colors.blue('Vouz allez directement à la case:', nouvellePosition))
+            return nouvellePosition; 
+        }
+    } 
+    else if ((positionPion === 9 && tour > 1) || positionPion === 18 || positionPion === 27 || positionPion === 36 || positionPion === 45 || positionPion === 54) 
+        {
+        nouvellePosition = positionPion + resultatDes
+        console.log(colors.blue('Ici une case “Oie”, ton pion est déplacé en fonction du résultat du lancé, '))
+        console.log(colors.blue('Vous êtes maintenant sur la case:', nouvellePosition));
+        return nouvellePosition; 
+    } 
+    else if (positionPion === 6 ) 
+    {
+        nouvellePosition = 12
+        console.log(colors.blue('Case "pont" allez directement à la case:', nouvellePosition))
+        return nouvellePosition; 
+    } 
+    else if (positionPion === 58 ) 
+    {
+        nouvellePosition = 1
+        console.log(colors.red('Case "Tête de mort" retournez en case:', nouvellePosition));
+        return nouvellePosition; 
+    } else {
+        return nouvellePosition
+    }
+}
+
+
+prompt.start();
+
+prompt.message = colors.rainbow("Jeu de L'Oie ");
+
+prompt.get([{ 
+    name: 'nom', 
+    description: colors.green('Quel est ton nom ?') 
+}], function (err, result) {
+    if (err) { 
+        console.log('il y a eu une erreur'); 
+    }
+    console.log(colors.cyan('Bonjour ' + result.nom + ', bienvenue dans le jeu !'));
+    affichePlateau ();
+    jouer();
+});
+
+
 let tour = 1;
-function jouer () {
-    console.log('\nTour :', tour)
-    
-    let resultatDes = lancerDes();
-    console.log("Résultat des dés:", resultatDes);
-    
-    positionPion = positionPion + resultatDes;
+function jouer (de1) {
+    prompt.get(colors.magenta("Veux-tu lancer les dés ?"), function (err, result) {
+        if (err) { 
+            console.log('il y a eu une erreur'); 
+        } else {
+            console.log('\nTour :', tour);
+            
+            let resultatDes = lancerDes();
+            console.log("Résultat des dés:", resultatDes);
+            
+            positionPion = positionPion + resultatDes;
+            console.log('vous êtes maintenant sur la case :', positionPion);
 
+            positionPion = casesSpeciales(de1, resultatDes, positionPion);
+            affichePlateau ()
 
-    //TODO: a changer en fonction du résultat de chaque dès
-    // if (tour === 1 && resultatDes === 9 ) {
-    //     positionPion = 53
-    //     console.log('Grâce à ton 9 à ce premier tour, ')
-    // }
-    console.log('vous êtes maintenant sur la case :', positionPion);
+            if(positionPion < 63) {
+                // affichePlateau ()
+                tour++;
+                jouer();
+            } else if (positionPion === 63) {
+                // affichePlateau ()
+                console.log(prompt.message = colors.rainbow('*** Vous avez gagné ! ***'));
+            } else if (positionPion > 63) {
+                console.log(colors.blue('Vous avez dépassé la case 63'));
+                positionPion = 63 - (positionPion - 63);
 
+                console.log(colors.blue('Vous êtes maintenant sur la case :', positionPion));
 
-    // cases spéciales
-    if (positionPion === 9 || positionPion === 18 || positionPion === 27 || positionPion === 36 || positionPion === 45 || positionPion === 54) {
-        positionPion = positionPion + resultatDes
-        console.log('Ici une case “oie”, tu peux avancer déplacer son pion en fonction du résultat du lancer, ')
+                affichePlateau ()
+                tour++;
+                jouer();
+                }
+            }
+        })
     }
-
-    if (positionPion === 6 ) {
-    positionPion = 12
-    console.log('Case "pont" aller directement à la case 12, ')
-    }
-
-    if (positionPion === 58 ) {
-        positionPion = 1
-        console.log('Case "Tête de mort" retourner en case 1, ')
-    }
-
-    if (positionPion > 63) {
-        console.log('Vous avez dépassé la case 63');
-        positionPion = 63 - (positionPion - 63);
-        console.log('Vous êtes maintenant sur la case :', positionPion); 
-    } else if (positionPion === 63) {
-        console.log('*** Vous avez gagné ! ***') ;
-    }
-    
-
-    // creation du plateau
-    let plateauDeJeu = creerPlateauJeuOie(positionPion);
-    console.log(plateauDeJeu);
-
-    if (positionPion === 63) {
-        return 'Vous avez gagné !';
-    }
-}
-
-jouer();
-// while (positionPion < 63) {
-//     jouer();
-//     tour++;
-//     if (positionPion < 63) {
-//         console.log('Veuillez passer au tour suivant');
-//     }
-// } 
-
-
-
-
-
