fd3c8b05b55d968512f825c0ee5833c8ce058c14
coyla <jcoyla123@gmail.com>
2024-03-06 12:33:33 +0100

    refacto functions

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -25,64 +25,112 @@ class Shop {
   agedBrie = Inventory.agedBrie;
   sulfuras = Inventory.sulfuras;
 
-  defaultUpdateQuality(item) {
+  defaultDecrementQuality(item) {
     if (item.quality > 0) {
+      //cette condition risque de poser probleme. peut être il faut l extraire ailleurs
       if (item.name != this.sulfuras) {
         item.quality = item.quality - 1;
       }
     }
   }
 
-  isItemThatIncreaseValueWhenIsOld(item) {
+  isVintageItem(item) {
     return item.name == this.agedBrie || item.name == this.backstagePass
+  }
+
+  incrementQuality(item) {
+    item.quality = item.quality + 1;
+  }
+
+  isItemExpired(item) {
+    return item.sellIn < 0
+  }
+
+  canIncrementQuality(item) {
+    return item.quality < this.maxQuality
+  }
+
+  isInExpirableItem(item) {
+    return item.name == this.sulfuras
   }
 
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      const item = this.items[i];
-      if (!this.isItemThatIncreaseValueWhenIsOld(item)) {
-        this.defaultUpdateQuality(item);
-      } else {
-        if (item.quality < this.maxQuality) {
-          item.quality = item.quality + 1;
-          if (item.name == this.backstagePass) {
-            if (item.sellIn < this.almostLastMinutePass) {
-              if (item.quality < this.maxQuality) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < this.lastMinutePass) {
-              if (item.quality < this.maxQuality) {
-                item.quality = item.quality + 1;
-              }
-            }
+    for (const item of this.items) {
+       //inverse le if et le else pour avoir if (this.isItemThatIncreaseValueWhenIsOld) ? 
+      //peut être plus lisible
+      if (this.isVintageItem(item)) {
+        //securité pour ne pas dépasser la limite max
+        if (this.canIncrementQuality(item)) {
+          //a améliorer ou refacto cette partie
+          if (this.isAgeBrieItem(item)) {
+            this.incrementQuality(item)
+          }
+          if (this.isBackStagePassItem(item)) {
+            this.incrementQuality(item)
+            item.quality = this.getQualityForBackstagePass(item);
           }
         }
       }
-      if (item.name != this.sulfuras) {
+      //faire la distinction entre classics iteams et sulfuras
+      else {
+        this.defaultDecrementQuality(item);
+      } 
+
+      //gene iteam au mieu de rien
+      if (!this.isInExpirableItem(item)) {
         item.sellIn = item.sellIn - 1;
       }
-      if (item.sellIn < 0) {
-        if (item.name != this.agedBrie) {
-          if (item.name != this.backstagePass) {
-            if (item.quality > 0) {
-              if (item.name != this.sulfuras) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
+
+      //fonction manageExpiredItem -> voir une class
+      if (this.isItemExpired(item)) {
+        //funciton is age abri
+        if (this.isAgeBrieItem(item)) {
+          //rajouter un test pour assurer qu on passe par incrementQuality en ayant un boolean
+          if (this.canIncrementQuality(item)) {
+            this.incrementQuality(item)
           }
-        } else {
-          if (item.quality < this.maxQuality) {
-            item.quality = item.quality + 1;
+        }
+        else {
+          if (this.isBackStagePassItem(item))
+          this.makeBackstagePassUnusable(item);
+          else {
+            this.defaultDecrementQuality(item)
           }
         }
       }
     }
 
     return this.items;
+  }
+
+  isBackStagePassItem(item) {
+    return item.name === this.backstagePass;
+  }
+
+  isAgeBrieItem(item) {
+    return item.name === this.agedBrie;
+  }
+
+  makeBackstagePassUnusable(item) {
+    item.quality = 0;
+  }
+
+  //peut etre faire une class BackStagePass qui prend un iteam et qui calcul le quality
+  getQualityForBackstagePass(item) {
+    let quality = item.quality;
+    if (item.sellIn < this.almostLastMinutePass) {
+      if (quality < this.maxQuality) {
+        quality = quality + 1;
+      }
+    }
+    if (item.sellIn < this.lastMinutePass) {
+      if (quality < this.maxQuality) {
+        quality = quality + 1;
+      }
+    }
+
+    return quality;
   }
 }
 
