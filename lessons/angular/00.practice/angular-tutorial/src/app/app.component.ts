5d1f690245a625c967b9ffed5a49a1b219546866
coyla <jcoyla123@gmail.com>
2024-06-13 12:40:43 +0200

    about section refactored works

--- lessons/angular/00.practice/angular-tutorial/src/app/app.component.ts
+++ lessons/angular/00.practice/angular-tutorial/src/app/app.component.ts
@@ -3,24 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tutorial';
 
   heroList = ['first', 'second'];
   currentIndex = 0;
-  imagesPath = ['/images/blue.jpg', '/images/red.png']
+  imagesPath = ['/images/blue.jpg', '/images/red.png'];
+
+  cards = [
+    {
+      text: 'New Release from Wondershare Filmora: For a Better Video Creation Experience',
+      img: '/images/about1.png',
+    },
+    {
+      text: 'AniSmall New Released : A Professional HD Multimedia File Compressor',
+      img: '/images/about2.png',
+    },
+    {
+      text: 'By sharing your inspiring story, you will uplift other women and earn Empowerment Funds!',
+      img: '/images/about3.png',
+    },
+  ];
 
   handleGoToInSlider(value: string) {
-    let nextIndex = value === "next" ? this.currentIndex + 1: this.currentIndex + -1;
-    if (value === "next") {
+    let nextIndex =
+      value === 'next' ? this.currentIndex + 1 : this.currentIndex + -1;
+    if (value === 'next') {
       if (nextIndex === this.heroList.length) {
         nextIndex = 0;
       }
-    } else if (value === "previous") {
+    } else if (value === 'previous') {
       if (nextIndex === -1) {
-        nextIndex = this.heroList.length -1;
+        nextIndex = this.heroList.length - 1;
       }
     }
     this.currentIndex = nextIndex;
--- lessons/angular/00.practice/angular-tutorial/src/app/components/about-card/about-card.component.ts
+++ lessons/angular/00.practice/angular-tutorial/src/app/components/about-card/about-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about-card',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './about-card.component.css'
 })
 export class AboutCardComponent {
+
+  @Input()
+  text = ""
+
+  @Input()
+  imgPath = ""
 
 }
